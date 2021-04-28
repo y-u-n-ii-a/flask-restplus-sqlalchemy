16b8a2dd2ca99c34bf1a54d88efb9b21b42bd5f1
Yuniia <yuniiaolkhova@gmail.com>
2021-04-28 14:02:41 +0300

    Separate courseModel

--- app.py
+++ app.py
@@ -1,36 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_restplus import Api, Resource, fields
 from db import db
+from course_service import CourseModel
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 api = Api(app)
 db.init_app(app)
-
-
-class CourseModel(db.Model):
-    __tablename__ = 'course'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100))
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
-    number_of_lectures = db.Column(db.Integer)
-
-    def __init__(self, name, number_of_lectures):
-        self.name = name
-        # self.start_date = start_date
-        # self.end_date = end_date
-        self.number_of_lectures = number_of_lectures
-
-    def __repr__(self):
-        return '<Course %r>' % self.id
-
-    def serialize(self):
-        return {'name': self.name,
-                'number_of_lectures': self.number_of_lectures
-                }
 
 
 course_model = api.model('Course', {
@@ -69,6 +46,7 @@ class CourseList(Resource):
         pass
 
 
+# TODO: handle error if the element doesn't exist
 @api.route('/course/<int:id>')
 class Course(Resource):
     @api.doc('Get course by id', params={'id': 'Id'})
---
+++ course_service.py
@@ -0,0 +1,26 @@
+from db import db
+
+
+class CourseModel(db.Model):
+    __tablename__ = 'course'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    number_of_lectures = db.Column(db.Integer)
+
+    def __init__(self, name, number_of_lectures):
+        self.name = name
+        # self.start_date = start_date
+        # self.end_date = end_date
+        self.number_of_lectures = number_of_lectures
+
+    def __repr__(self):
+        return '<Course %r>' % self.id
+
+# TODO: correct serialization
+    def serialize(self):
+        return {'name': self.name,
+                'number_of_lectures': self.number_of_lectures
+                }
