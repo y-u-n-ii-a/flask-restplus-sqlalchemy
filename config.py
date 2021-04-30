b43fc1e34cd1ffdca6b0df3a81b27df5522ad83d
Yuniia <yuniiaolkhova@gmail.com>
2021-04-30 23:42:35 +0300

    Update config with new db uri

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-# import os
-#
-# basedir = os.path.dirname(__file__)
+import os
+
+basedir = os.path.dirname(__file__)
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///courses.db'
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'courses.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'courses.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
